d8d9cd110a27d234aaa61e85578255f3b101e11f
Dionisis Nikas <dionisisnikas@gmail.com>
2021-09-30 20:44:55 +0300

    files

---
+++ Cur-Culator/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Cur-Culator
+//
+//  Created by Dennis Nikas on 30/9/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Cur-Culator/Cur_CulatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cur_CulatorApp.swift
+//  Cur-Culator
+//
+//  Created by Dennis Nikas on 30/9/21.
+//
+
+import SwiftUI
+
+@main
+struct Cur_CulatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
